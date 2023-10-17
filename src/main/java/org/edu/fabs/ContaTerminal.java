2478f540551a67b8fbcef1c2ed503a99a47fae48
bianavic <bianavic@gmail.com>
2023-10-17 20:41:39 -0300

    adiconado comentario documentando etapas da implementacao

--- src/main/java/org/edu/fabs/ContaTerminal.java
+++ src/main/java/org/edu/fabs/ContaTerminal.java
@@ -1,7 +1,14 @@
 package org.edu.fabs;
 
 public class ContaTerminal {
+
     public static void main(String[] args) {
+
+        // TODO: conhecer e importar a classe Scanner
+        // exibir mensagens para o usuario
+        // obter pela scanner os valores digitados no terminal
+        // exibir a mensagem conta criada
         System.out.printf("Hello and welcome!");
     }
+
 }
